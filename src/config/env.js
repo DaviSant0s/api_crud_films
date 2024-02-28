50d6246e899de428a9b23e76b0b74a1cdedb0f7a
Davi Santos <daviir17@gmail.com>
2024-02-28 15:05:11 -0300

    fix timestamp

---
+++ src/config/env.js
@@ -0,0 +1,9 @@
+// Ele vai começar a executar e entender as variáveis ambiente, conseguir acessá-las a
+// partir desse arquivo
+
+require('dotenv').config();
+
+module.exports = {
+  PORT: process.env.PORT,
+  JWT_SECRET: process.env.JWT_SECRET,
+}
